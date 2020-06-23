b23e5c6dbef2f6f3e1610afc7269ac7f4ad4f2ac
Paul Schreiber <paulschreiber@gmail.com>
2020-06-23 18:39:11 -0400

    update specs to be compatible with FactoryBot > 5.0.0

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,11 +4,11 @@ FactoryBot.define do
 
 
   factory :user do
-    email "test@example.com"
+    email { "test@example.com" }
 
     factory :admin do
-      admin true
-      email "admin@example.com"
+      admin { true }
+      email { "admin@example.com" }
     end
   end
 
@@ -16,33 +16,33 @@ FactoryBot.define do
     slack_username
     slack_user_id
     started_on  { Date.today }
-    time_zone "Eastern Time (US & Canada)"
+    time_zone { "Eastern Time (US & Canada)" }
   end
 
   factory :sent_message do
     employee
     association :message, factory: :onboarding_message
-    message_body "Message body!"
+    message_body { "Message body!" }
     sent_on { Date.today }
     sent_at { Time.parse("10:00:00 UTC") }
   end
 
   factory :onboarding_message do
-    title "Onboarding message 1"
-    body "This is an awesome onboarding message!"
-    days_after_start 3
+    title { "Onboarding message 1" }
+    body { "This is an awesome onboarding message!" }
+    days_after_start { 3 }
     time_of_day { Time.parse("10:00:00 UTC") }
-    tag_list "test_tag, test_tag_two"
+    tag_list { "test_tag, test_tag_two" }
   end
 
   factory :quarterly_message do
-    title "Quarterly message 1"
-    body "This is an awesome quarterly message!"
-    tag_list "test_tag, test_tag_two"
+    title { "Quarterly message 1" }
+    body { "This is an awesome quarterly message!" }
+    tag_list { "test_tag, test_tag_two" }
   end
 
   factory :broadcast_message do
-    title "Message to everyone"
-    body "Everyone needs to know this thing!"
+    title { "Message to everyone" }
+    body { "Everyone needs to know this thing!" }
   end
 end
