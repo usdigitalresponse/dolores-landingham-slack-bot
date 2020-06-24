ecc84e89088304f391c9b71aa23dbd9600571f83
Paul Schreiber <paulschreiber@gmail.com>
2020-06-24 11:10:36 -0400

    use client.conversations_open() instead of client.im_open(), which is deprecated

--- app/services/slack_channel_id_finder.rb
+++ app/services/slack_channel_id_finder.rb
@@ -2,7 +2,7 @@ class SlackChannelIdFinder < SlackApiWrapper
   def run
     if slack_user_by_id
       slack_user_id = slack_user["id"]
-      chat = client.im_open(user: slack_user_id)
+      chat = client.conversations_open(users: slack_user_id)
       chat["channel"]["id"]
     end
   end
