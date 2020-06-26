44d7fb23819f2e7004ad928ce2121e2bfe1254d0
Paul Schreiber <paulschreiber@gmail.com>
2020-06-26 11:33:20 -0400

    show error if no email found

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,10 +3,14 @@ class AuthController < ApplicationController
 
   def oauth_callback
     if team_member?
-      user = User.find_or_create_by(email: auth_email)
-      sign_in(user)
-      flash[:success] = I18n.t('controllers.auth_controller.successes.oauth_callback')
-      redirect_to root_path
+      if auth_email.present?
+        user = User.find_or_create_by(email: auth_email)
+        sign_in(user)
+        flash[:success] = I18n.t('controllers.auth_controller.successes.oauth_callback')
+        redirect_to root_path
+      else
+        render text: "Please make sure your email address is public on your GitHub profile"
+      end
     else
       render :forbidden
     end
