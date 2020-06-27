0250376859b0dee5fec106ba152fb9f6f0397c27
Paul Schreiber <paulschreiber@gmail.com>
2020-06-26 21:01:34 -0400

    enable production debugging via environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   config.middleware.use Rack::CanonicalHost, ENV.fetch("APPLICATION_HOST")
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = ActiveModel::Type::Boolean.new.cast(ENV["DEBUG_MODE"] || 0)
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor = :uglifier
